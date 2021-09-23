d9fc620d2c1c288113c485cf8853e33e991cb570
Scott Pakin <scott+gpg@pakin.org>
2021-09-23 01:10:28 -0600

    Add support for transforms to all shapes

--- simple-py-api.py
+++ simple-py-api.py
@@ -59,60 +59,60 @@ def style(**kwargs):
         else:
             _default_style[k] = str(v)
 
-def circle(center, r, **style):
+def circle(center, r, transform="", **style):
     'Draw a circle.'
     obj = inkex.Circle(cx=str(center[0]), cy=str(center[1]), r=str(r),
-                       style=_construct_style(style))
+                       transform=transform, style=_construct_style(style))
     _simple_objs.append(obj)
 
-def ellipse(center, rx, ry, **style):
+def ellipse(center, rx, ry, transform="", **style):
     'Draw an ellipse.'
     obj = inkex.Ellipse(cx=str(center[0]), cy=str(center[1]),
                         rx=str(rx), ry=str(ry),
-                        style=_construct_style(style))
+                        transform=transform, style=_construct_style(style))
     _simple_objs.append(obj)
 
-def rect(ul, lr, **style):
+def rect(ul, lr, transform="", **style):
     'Draw a rectangle.'
     wd = lr[0] - ul[0]
     ht = lr[1] - ul[1]
     obj = inkex.Rectangle(x=str(ul[0]), y=str(ul[1]),
                           width=str(wd), height=str(ht),
-                          style=_construct_style(style))
+                          transform=transform, style=_construct_style(style))
     _simple_objs.append(obj)
 
-def line(p1, p2, **style):
+def line(p1, p2, transform, **style):
     'Draw a line.'
     obj = inkex.Line(x1=str(p1[0]), y1=str(p1[1]),
                      x2=str(p2[0]), y2=str(p2[1]),
-                     style=_construct_style(style))
+                     transform=transform, style=_construct_style(style))
     _simple_objs.append(obj)
 
-def polyline(*coords, **style):
+def polyline(*coords, transform="", **style):
     'Draw a polyline.'
     if len(coords) < 2:
         inkex.utils.errormsg('A polyline must contain at least two points.')
         return
     pts = ' '.join(["%s,%s" % (str(x), str(y)) for x, y in coords])
-    obj = inkex.Polyline(points=pts, style=_construct_style(style))
+    obj = inkex.Polyline(points=pts, transform=transform, style=_construct_style(style))
     _simple_objs.append(obj)
 
-def polygon(*coords, **style):
+def polygon(*coords, transform="", **style):
     'Draw a polygon.'
     if len(coords) < 3:
         inkex.utils.errormsg('A polygon must contain at least three points.')
         return
     pts = ' '.join(["%s,%s" % (str(x), str(y)) for x, y in coords])
-    obj = inkex.Polygon(points=pts, style=_construct_style(style))
+    obj = inkex.Polygon(points=pts, transform=transform, style=_construct_style(style))
     _simple_objs.append(obj)
 
-def path(*elts, **style):
+def path(*elts, transform="", **style):
     'Draw an arbitrary path.'
     if len(elts) == 0:
         inkex.utils.errormsg('A path must contain at least one path element.')
         return
     d = ' '.join([str(e) for e in elts])
-    obj = inkex.PathElement(d=d, style=_construct_style(style))
+    obj = inkex.PathElement(d=d, transform=transform, style=_construct_style(style))
     _simple_objs.append(obj)
 
 # ----------------------------------------------------------------------
