a761ee374a1df85db8de505dcacbccbd10b06264
Scott Pakin <scott+gpg@pakin.org>
2021-09-25 15:33:27 -0600

    Present a binary-tree example and a Sierpinski-triangle example

---
+++ examples/binary_tree.py
@@ -0,0 +1,28 @@
+# Use Simple Inkscape Scripting to draw a binary tree.  The canvas
+# should be about as wide as A4 or U.S. Letter paper to avoid node
+# overlap in the deepest level of the tree.
+
+global tree_from  # Required for recursive functions
+
+
+def tree_from(cx, cy, rad, hsep, vsep, depth, max_depth):
+    'Recursively draw a binary tree.'
+    left, right = None, None
+    if depth < max_depth:
+        left = tree_from(cx - hsep, cy + vsep, rad,
+                         hsep/2, vsep,
+                         depth + 1, max_depth)
+        right = tree_from(cx + hsep, cy + vsep, rad,
+                          hsep/2, vsep,
+                          depth + 1, max_depth)
+    c = circle((cx, cy), rad)
+    if left is not None:
+        connector(c, left, fill='none')
+        connector(c, right, fill='none')
+    return c
+
+
+style(stroke='black', stroke_width=2, fill='#f4e3d7')
+tree_from(cx=width/2, cy=20, rad=10,
+          hsep=width/4, vsep=50,
+          depth=0, max_depth=5)
---
+++ examples/sierpinski_triangle.py
@@ -0,0 +1,29 @@
+# Use Simple Inkscape Scripting to draw a colorful Sierpinski triangle.
+
+global eq_triangle, sierpinski
+
+
+def eq_triangle(tx, ty, edge, **style):
+    """Draw an equilateral triangle with a given top coordinate and
+    edge position.  Return the triangle's height."""
+    ht = sqrt(3)*edge/2
+    polygon((tx, ty), (tx + edge/2, ty + ht), (tx - edge/2, ty + ht), **style)
+    return ht
+
+
+def sierpinski(tx, ty, edge, depth, colors):
+    '''Draw a Sierpinski triangle to a given depth until a set of colors
+    runs out.'''
+    max_depth = len(colors)
+    if depth >= max_depth:
+        return
+    ht = eq_triangle(tx, ty, edge, fill=colors[depth % len(colors)])
+    sierpinski(tx, ty, edge/2, depth + 1, colors)
+    sierpinski(tx - edge/4, ty + ht/2, edge/2, depth + 1, colors)
+    sierpinski(tx + edge/4, ty + ht/2, edge/2, depth + 1, colors)
+
+
+style(stroke='black', stroke_width=1)
+edge = min(width, height)/2
+colors = ['#ff0000', '#00ff00', '#0000ff', '#00ffff', '#ff00ff', '#ffff00']
+sierpinski(width/2, 50, edge, 0, colors)
