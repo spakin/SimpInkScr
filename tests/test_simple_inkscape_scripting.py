0bf37f6e48673c47969f61cb7e825eaa5587a8de
Scott Pakin <scott+gpg@pakin.org>
2022-01-24 19:13:50 -0700

    Test examples from the Modifying Existing Objects wiki page

--- tests/test_simple_inkscape_scripting.py
+++ tests/test_simple_inkscape_scripting.py
@@ -4,8 +4,11 @@ from inkex.tester import ComparisonMixin, InkscapeExtensionTestMixin, TestCase
 from inkex.tester.filters import CompareOrderIndependentStyle
 
 class SimpInkScrBasicTest(ComparisonMixin, InkscapeExtensionTestMixin, TestCase):
+    # Indicate how testing should be performed.
     effect_class = SimpleInkscapeScripting
     compare_filters = [CompareOrderIndependentStyle()]
+
+    # Define a few helper strings.
     animation_base = '''\
 def make_rect(center, fill, edge=100):
     return rect(inkex.Vector2d(-edge/2, -edge/2) + center,
@@ -16,6 +19,14 @@ r1 = make_rect((50, 50), '#aade87')
 r2 = make_rect((width/2, height/2), '#9955ff')
 r3 = make_rect((width - 50, height - 50), '#d35f5f')
 '''
+    blue_red = '''
+blue = rect((90, 0), (170, 50), fill='#55ddff')
+red = rect((0, 0), (80, 50), fill='#ff5555')
+'''
+
+    # Define all of the tests to run.  Simple Inkscape Scripting is a
+    # large, featureful extension so many tests are needed to achieve even
+    # modest coverage of all the code paths.
     comparisons = [
         # The following tests come from the Shape Construction wiki page.
         ('--program=circle((width/2, height/2), 50)',),
@@ -142,6 +153,18 @@ r4 = make_rect((0, 0), '#5599ff')
 r4.transform = 'translate(%.5f, %.5f) rotate(200) scale(2)' % (width/2, height/2)
 r1.animate(r4, duration='3s')
 ''',),
+
+        # The following tests come from the Modifying Existing Objects wiki
+        # page.
+        ('--program=%s\nred.translate((50, 30))' % blue_red,),
+        ('--program=%s\nred.rotate(30)' % blue_red,),
+        ('--program=%s\nred.rotate(30, (85, 25))' % blue_red,),
+        ("--program=%s\nred.rotate(30, 'll')" % blue_red,),
+        ('--program=%s\nred.scale(1.5)' % blue_red,),
+        ('--program=%s\nred.scale((0.75, 1.5))' % blue_red,),
+        ("--program=%s\nred.scale(1.5, 'ur')" % blue_red,),
+        ('--program=%s\nred.skew((10, 0))' % blue_red,),
+        ("--program=%s\nred.skew((0, 10), 'lr')" % blue_red,),
 
         # The following are additional tests intended to increase coverage.
         ('''--program=
