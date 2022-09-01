26089763189f1fef18dfeda90503df5655c230ea
shark <inter1965@qq.com>
2022-09-01 17:13:27 +0800

    restructure the directory.

---
+++ simpinkscr/__init__.py
@@ -0,0 +1,10 @@
+from math import *
+from random import *
+from inkex.paths import Arc, Curve, Horz, Line, Move, Quadratic, Smooth, \
+    TepidQuadratic, Vert, ZoneClose
+from .simple_inkscape_scripting import style, transform, circle, ellipse, \
+    rect, line, polyline, polygon, regular_polygon, star, arc, path, \
+    connector, text, image, clone, duplicate, group, layer, hyperlink, \
+    inkex_object, filter_effect, linear_gradient, radial_gradient, \
+    clip_path, massk, marker, push_defaults, pop_defaults, path_effect, \
+    selected_shapes, all_shapes, guide
