8469b2fc59893b83432c6a80a5de49406d6e56cb
radu <rf@remotedots.com>
2023-07-03 23:37:03 +0200

    Implement the label parameter in the guide lines.

--- simpinkscr/svg_to_simp_ink_script.py
+++ simpinkscr/svg_to_simp_ink_script.py
@@ -1022,9 +1022,15 @@ class SvgToPythonScript(inkex.OutputExtension):
         if color is not None:
             extra = ', color=%s' % repr(color)
 
+        # Determine if we were given a label.
+        extra_label = ''
+        label = node.get('inkscape:label')
+        if label is not None:
+            extra_label = ', label=%s' % repr(label)
+
         # Generate code and wrap it in a statement.
         code = ['guides.append(guide((%.10g, %.10g), %.10g%s))' %
-                (pos[0], pos[1], angle, extra)]
+                (pos[0], pos[1], angle, extra, extra_label)]
         return self.Statement(code, node.get_id(), [])
 
     def convert_all_shapes(self):
