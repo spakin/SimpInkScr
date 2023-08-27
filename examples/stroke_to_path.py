15777f235afec5fb883b02a90778a155dbe54497
Scott Pakin <scott+gpg@pakin.org>
2023-08-27 11:43:57 -0600

    Include an example of apply_action

---
+++ examples/stroke_to_path.py
@@ -0,0 +1,14 @@
+############################################################
+# Show how Simple Inkscape Scripting can invoke Inkscape's #
+# *Path* → *Stroke to Path* function.                      #
+############################################################
+
+r = rect((100, 100), (200, 200),
+         stroke_width=16, stroke='#000080', fill='#add8e6')
+new_objs = apply_action('object-stroke-to-path', r)
+for obj in new_objs:
+    try:
+        if obj.style()['fill'] == '#000080':
+            obj.translate_path((50, 50))
+    except KeyError:
+        pass
