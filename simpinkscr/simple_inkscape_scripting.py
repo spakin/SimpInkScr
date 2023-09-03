spakin/SimpInkScr

commit fc3acb05d7f91299cc5b1fcf677ef3c71015139b

Update the code to work with Inkscape 1.3

simpinkscr/simple_inkscape_scripting.py
@@ -1644,8 +1644,14 @@ class SimpleGuide(SVGOutputMixin):
         global _simple_top
         self._pos = pos
         self._angle = angle
-        pos = (pos[0], _simple_top.canvas.height - pos[1])
-        self._inkscape_obj.move_to(pos[0], pos[1], 180 - angle)
+        try:
+            # Inkscape 1.3+
+            nv = _simple_top.svg_root.namedview
+            self._inkscape_obj = nv.add_guide((pos[0], pos[1]), float(angle))
+        except AttributeError:
+            # Older Inkscapes
+            pos = (pos[0], _simple_top.canvas.height - pos[1])
+            self._inkscape_obj.move_to(pos[0], pos[1], 180 - angle)
 
     @property
     def position(self):
@@ -1699,11 +1705,8 @@ class SimpleGuide(SVGOutputMixin):
     def _from_inkex_object(self, iobj):
         '''Create a Simple Inkscape Scripting SimpleGuide from an inkex
         Guide object.'''
-        # Convert the point from the pre-Inkscape 1.0 coordinate system.
-        pt = iobj.point
-        pos = (pt.x, _simple_top.canvas.height - pt.y)
-
         # Compute the angle at which the guide is oriented.
+        pos = iobj.position
         try:
             # Inkscape 1.2+
             angle = 90 - math.degrees(iobj.orientation.angle)
@@ -3262,6 +3265,12 @@ def apply_action(action, obj=None):
             # Object type is not recognized by Simple Inkscape Scripting.
             pass
 
+    # Sort new_sis_objs by the underlying inkex objects' order in the SVG
+    # file.
+    svg_order = {iobj: num
+                 for iobj, num in zip(svg_root.iter(), range(2**31))}
+    new_sis_objs.sort(key=lambda k: svg_order[k.get_inkex_object()])
+
     # Assign a parent to each created object.
     iobj2obj = {o.get_inkex_object(): o for o in all_objs + new_sis_objs}
     for obj in new_sis_objs:
@@ -3281,7 +3290,7 @@ def apply_action(action, obj=None):
             # nothing and hope for the best.
             pass
 
-    # Return the set of newly created objects.
+    # Return the list of newly created objects.
     return new_sis_objs
 
 

.gitignore
@@ -3,7 +3,6 @@
 /*.egg-info
 __pycache__/
 /sbdist
-tests/data/cmd/
 tests/data/refs/simple_inkscape_scripting*.out.export
 tests/data/refs/svg_to_simp_ink_script*.out.export
 tests/__pycache__/

simpinkscr/svg_to_simp_ink_script.py
@@ -767,7 +767,7 @@ class SvgToPythonScript(inkex.OutputExtension):
             # The stop offset is a mandatory field.
             if not isinstance(stop, inkex.Stop):
                 continue
-            stop_args = [str(self._svg_str_to_python(stop.offset))]
+            stop_args = [str(self._svg_str_to_python(str(stop.offset)))]
 
             # stop-color and stop-opacity can be expressed directly or
             # within a style.  We therefore have to look in both places.

tests/data/cmd/inkscape/220defc27cb646ba93c40874dbe48bf8.msg
@@ -1,0 +1,55 @@
+Content-Type: multipart/mixed; boundary="--CALLDATA--//--CALLDATA--"
+MIME-Version: 1.0
+Program: inkscape
+Arguments: --actions=select-by-id:rect5815;object-stroke-to-path;export-filename:input.svg;export-overwrite;export-do;quit-immediate input.svg --batch-process --app-id-tag=lBbCyIkpau
+
+----CALLDATA--//--CALLDATA--
+Content-Type: text/plain; charset="utf-8"
+MIME-Version: 1.0
+Content-Transfer-Encoding: base64
+
+WW91IHNob3VsZCBub3QgdXNlIEFwcEltYWdlIGluIHByb2R1Y3Rpb24sIGJ1dCB5b3UgY2FuIHNw
+ZWVkdXAgdGhlIEFwcEltYWdlIGJ5IGZvbGxvd2luZyB0aGlzIGd1aWRlOiBodHRwczovL2lua3Nj
+YXBlLm9yZy9sZWFybi9hcHBpbWFnZS8K
+
+----CALLDATA--//--CALLDATA--
+Content-Type: application/octet-stream; Name="input.svg"
+MIME-Version: 1.0
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment
+Filename: input.svg
+
+PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+Cjwh
+LS0gQ3JlYXRlZCB3aXRoIElua3NjYXBlIChodHRwOi8vd3d3Lmlua3NjYXBlLm9yZy8pIC0tPgoK
+PHN2ZwogICB3aWR0aD0iNzkzLjcwMDc4NzQiCiAgIGhlaWdodD0iMTEyMi41MTk2ODUiCiAgIGlk
+PSJzdmc4NTU1IgogICB2ZXJzaW9uPSIxLjEiCiAgIHNvZGlwb2RpOmRvY25hbWU9ImlucHV0LnN2
+ZyIKICAgeG1sbnM6aW5rc2NhcGU9Imh0dHA6Ly93d3cuaW5rc2NhcGUub3JnL25hbWVzcGFjZXMv
+aW5rc2NhcGUiCiAgIHhtbG5zOnNvZGlwb2RpPSJodHRwOi8vc29kaXBvZGkuc291cmNlZm9yZ2Uu
+bmV0L0RURC9zb2RpcG9kaS0wLmR0ZCIKICAgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAv
+c3ZnIgogICB4bWxuczpzdmc9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIgogICB4bWxuczpy
+ZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiCiAgIHhtbG5z
+OmNjPSJodHRwOi8vY3JlYXRpdmVjb21tb25zLm9yZy9ucyMiCiAgIHhtbG5zOmRjPSJodHRwOi8v
+cHVybC5vcmcvZGMvZWxlbWVudHMvMS4xLyI+CiAgPGRlZnMKICAgICBpZD0iZGVmczUzOTIiIC8+
+CiAgPHNvZGlwb2RpOm5hbWVkdmlldwogICAgIGlkPSJiYXNlIgogICAgIHBhZ2Vjb2xvcj0iI2Zm
+ZmZmZiIKICAgICBib3JkZXJjb2xvcj0iIzY2NjY2NiIKICAgICBib3JkZXJvcGFjaXR5PSIxLjAi
+CiAgICAgaW5rc2NhcGU6cGFnZW9wYWNpdHk9IjAuMCIKICAgICBpbmtzY2FwZTpwYWdlc2hhZG93
+PSIyIgogICAgIGlua3NjYXBlOnpvb209IjAuMzUiCiAgICAgaW5rc2NhcGU6Y3g9IjM3NSIKICAg
+ICBpbmtzY2FwZTpjeT0iNTIwIgogICAgIGlua3NjYXBlOmRvY3VtZW50LXVuaXRzPSJweCIKICAg
+ICBpbmtzY2FwZTpjdXJyZW50LWxheWVyPSJsYXllcjEiCiAgICAgaW5rc2NhcGU6c2hvd3BhZ2Vz
+aGFkb3c9IjIiCiAgICAgaW5rc2NhcGU6cGFnZWNoZWNrZXJib2FyZD0iMCIKICAgICBpbmtzY2Fw
+ZTpkZXNrY29sb3I9IiNkMWQxZDEiIC8+CiAgPG1ldGFkYXRhCiAgICAgaWQ9Im1ldGFkYXRhOTYw
+MyI+CiAgICA8cmRmOlJERj4KICAgICAgPGNjOldvcmsKICAgICAgICAgcmRmOmFib3V0PSIiPgog
+ICAgICAgIDxkYzpmb3JtYXQ+aW1hZ2Uvc3ZnK3htbDwvZGM6Zm9ybWF0PgogICAgICAgIDxkYzp0
+eXBlCiAgICAgICAgICAgcmRmOnJlc291cmNlPSJodHRwOi8vcHVybC5vcmcvZGMvZGNtaXR5cGUv
+U3RpbGxJbWFnZSIgLz4KICAgICAgPC9jYzpXb3JrPgogICAgPC9yZGY6UkRGPgogIDwvbWV0YWRh
+dGE+CiAgPGcKICAgICBpbmtzY2FwZTpsYWJlbD0iTGF5ZXIgMSIKICAgICBpbmtzY2FwZTpncm91
+cG1vZGU9ImxheWVyIgogICAgIGlkPSJsYXllcjEiPgogICAgPGcKICAgICAgIGlkPSJyZWN0NTgx
+NSI+CiAgICAgIDxwYXRoCiAgICAgICAgIHN0eWxlPSJjb2xvcjojMDAwMDAwO2ZpbGw6I2FkZDhl
+NjstaW5rc2NhcGUtc3Ryb2tlOm5vbmUiCiAgICAgICAgIGQ9Ik0gMTAwLDEwMCBIIDIwMCBWIDIw
+MCBIIDEwMCBaIgogICAgICAgICBpZD0icGF0aDEiIC8+CiAgICAgIDxwYXRoCiAgICAgICAgIHN0
+eWxlPSJjb2xvcjojMDAwMDAwO2ZpbGw6IzAwMDA4MDstaW5rc2NhcGUtc3Ryb2tlOm5vbmUiCiAg
+ICAgICAgIGQ9Ik0gOTIsOTIgViAxMDAgMjA4IEggMjA4IFYgOTIgWiBNIDEwOCwxMDggSCAxOTIg
+ViAxOTIgSCAxMDggWiIKICAgICAgICAgaWQ9InBhdGgyIiAvPgogICAgPC9nPgogIDwvZz4KPC9z
+dmc+Cg==
+
+----CALLDATA--//--CALLDATA----

tests/data/refs/sis__009fa6028769d24916a9e6032dc9d122.out
@@ -1,4 +1,4 @@
-<!-- Created with Inkscape (http://www.inkscape.org/) --><svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" width="793.7007874" height="1122.519685">
+<!-- Created with Inkscape (http://www.inkscape.org/) --><svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" width="793.7007874" height="1122.519685" id="svg6654">
   <defs/>
   <sodipodi:namedview id="base" pagecolor="#ffffff" bordercolor="#666666" borderopacity="1.0" inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:zoom="0.35" inkscape:cx="375" inkscape:cy="520" inkscape:document-units="px" inkscape:current-layer="layer1"/>
   <metadata>
@@ -9,5 +9,5 @@
       </cc:Work>
     </rdf:RDF>
   </metadata>
-  <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1"><rect x="90" y="0" width="80" height="50" style="stroke:black;fill:#55ddff"/><rect x="0" y="0" width="80" height="50" style="stroke:black;fill:#ff5555" transform="matrix(1 0.176327 0 1 0 -14.1062)"/></g>
+  <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1"><rect x="90" y="0" width="80" height="50" style="stroke:black;fill:#55ddff"/><rect x="0" y="0" width="80" height="50" style="stroke:black;fill:#ff5555" id="rect4780" transform="matrix(1 0.176327 0 1 0 -14.1062)"/></g>
 </svg>

tests/data/refs/sis__0b76cdb52daf3cd259f54efc1e0643b7.out
@@ -1,4 +1,4 @@
-<!-- Created with Inkscape (http://www.inkscape.org/) --><svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" width="793.7007874" height="1122.519685">
+<!-- Created with Inkscape (http://www.inkscape.org/) --><svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" width="793.7007874" height="1122.519685" id="svg8555">
   <defs/>
   <sodipodi:namedview id="base" pagecolor="#ffffff" bordercolor="#666666" borderopacity="1.0" inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:zoom="0.35" inkscape:cx="375" inkscape:cy="520" inkscape:document-units="px" inkscape:current-layer="layer1"/>
   <metadata>
@@ -9,5 +9,5 @@
       </cc:Work>
     </rdf:RDF>
   </metadata>
-  <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1"><rect x="90" y="0" width="80" height="50" style="stroke:black;fill:#55ddff"/><rect x="0" y="0" width="80" height="50" style="stroke:black;fill:#ff5555" transform="matrix(0.866025 0.5 -0.5 0.866025 17.859 -16.6506)"/></g>
+  <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1"><rect x="90" y="0" width="80" height="50" style="stroke:black;fill:#55ddff"/><rect x="0" y="0" width="80" height="50" style="stroke:black;fill:#ff5555" id="rect5815" transform="matrix(0.866025 0.5 -0.5 0.866025 17.859 -16.6506)"/></g>
 </svg>

tests/data/refs/sis__10926e00f595eea8f68837c808aae5d9.out
@@ -1,4 +1,4 @@
-<!-- Created with Inkscape (http://www.inkscape.org/) --><svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" width="793.7007874" height="1122.519685">
+<!-- Created with Inkscape (http://www.inkscape.org/) --><svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" width="793.7007874" height="1122.519685" id="svg5347">
   <defs/>
   <sodipodi:namedview id="base" pagecolor="#ffffff" bordercolor="#666666" borderopacity="1.0" inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:zoom="0.35" inkscape:cx="375" inkscape:cy="520" inkscape:document-units="px" inkscape:current-layer="layer1"/>
   <metadata>
@@ -9,5 +9,5 @@
       </cc:Work>
     </rdf:RDF>
   </metadata>
-  <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1"><rect x="90" y="0" width="80" height="50" style="stroke:black;fill:#55ddff"/><rect x="0" y="0" width="80" height="50" style="stroke:black;fill:#ff5555" transform="matrix(1.5 0 0 1.5 -40 0)"/></g>
+  <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1"><rect x="90" y="0" width="80" height="50" style="stroke:black;fill:#55ddff"/><rect x="0" y="0" width="80" height="50" style="stroke:black;fill:#ff5555" id="rect2304" transform="matrix(1.5 0 0 1.5 -40 0)"/></g>
 </svg>

tests/data/refs/sis__250a96e4775a8c431f825e7ed9f17750.out
@@ -1,6 +1,6 @@
 <!-- Created with Inkscape (http://www.inkscape.org/) --><svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" width="105mm" height="148mm" viewBox="0.0 0.0 396.8503937007874 559.3700787401575">
   <defs/>
-  <sodipodi:namedview id="base" pagecolor="#ffffff" bordercolor="#666666" borderopacity="1.0" inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:zoom="0.35" inkscape:cx="375" inkscape:cy="520" inkscape:document-units="px" inkscape:current-layer="layer1"><inkscape:page width="396.8503937007874" height="559.3700787401575" x="0.0" y="0.0" inkscape:label="1"/><inkscape:page width="396.8503937007874" height="559.3700787401575" x="396.8503937007874" y="0.0" inkscape:label="2"/><inkscape:page width="396.8503937007874" height="559.3700787401575" x="0.0" y="559.3700787401575" inkscape:label="3"/><inkscape:page width="396.8503937007874" height="559.3700787401575" x="396.8503937007874" y="559.3700787401575" inkscape:label="4"/></sodipodi:namedview>
+  <sodipodi:namedview id="base" pagecolor="#ffffff" bordercolor="#666666" borderopacity="1.0" inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:zoom="0.35" inkscape:cx="375" inkscape:cy="520" inkscape:document-units="px" inkscape:current-layer="layer1"><inkscape:page x="0" y="0" width="396.8503937007874" height="559.3700787401575"/><inkscape:page width="396.8503937007874" height="559.3700787401575" x="0.0" y="0.0" inkscape:label="1"/><inkscape:page width="396.8503937007874" height="559.3700787401575" x="396.8503937007874" y="0.0" inkscape:label="2"/><inkscape:page width="396.8503937007874" height="559.3700787401575" x="0.0" y="559.3700787401575" inkscape:label="3"/><inkscape:page width="396.8503937007874" height="559.3700787401575" x="396.8503937007874" y="559.3700787401575" inkscape:label="4"/></sodipodi:namedview>
   <metadata>
     <rdf:RDF>
       <cc:Work rdf:about="">

commit 7e3a37eae37185eac90e7d4dd3a88711d07af1a8

Import guides in Inkscape 1.2
As part of its attempt to make Simple Inkscape Scripting work with
Inkscape 1.3, fc3acb05d7f91299cc5b1fcf677ef3c71015139b broke Inkscape
1.2 compatibility in terms of creating a Simple Inkscape Scripting
`SimpleGuide` from a guide read from an existing SVG file.  This
commit restores Inkscape 1.2 compatibility and thereby closes #101.

simpinkscr/simple_inkscape_scripting.py
@@ -1707,8 +1707,16 @@ class SimpleGuide(SVGOutputMixin):
     def _from_inkex_object(self, iobj):
         '''Create a Simple Inkscape Scripting SimpleGuide from an inkex
         Guide object.'''
+        # Find the guide's anchor point.
+        try:
+            # Inkscape 1.3+
+            pos = iobj.position
+        except AttributeError:
+            # Inkscape 1.2 and earlier
+            pt = iobj.point
+            pos = (pt.x, _simple_top.canvas.height - pt.y)
+
         # Compute the angle at which the guide is oriented.
-        pos = iobj.position
         try:
             # Inkscape 1.2+
             angle = 90 - math.degrees(iobj.orientation.angle)