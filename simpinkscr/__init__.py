9c5a5216297e5f38c63df4e824bf46f345d43fca
Scott Pakin <scott+gpg@pakin.org>
2023-12-24 22:46:42 -0700

    Update the list of script-callable SIS functions

--- simpinkscr/__init__.py
+++ simpinkscr/__init__.py
@@ -4,9 +4,10 @@ from inkex import Transform
 from inkex.paths import Arc, Curve, Horz, Line, Move, Quadratic, Smooth, \
     TepidQuadratic, Vert, ZoneClose
 from .simple_inkscape_scripting import all_pages, all_shapes, \
-    apply_path_operation, arc, circle, clip_path, clone, connector, \
-    duplicate, ellipse, filter_effect, foreign, group, guide, hyperlink, \
-    image, inkex_object, layer, linear_gradient, line, main, marker, mask, \
-    objects_from_svg_file, page, path_effect, path, polygon, polyline, \
-    pop_defaults, push_defaults, radial_gradient, randcolor, rect, \
-    regular_polygon, save_file, selected_shapes, star, style, text, transform
+    apply_action, apply_path_operation, arc, circle, clip_path, clone, \
+    connector, duplicate, ellipse, filter_effect, foreign, group, \
+    guide, hyperlink, image, inkex_object, layer, linear_gradient, \
+    line, main, marker, mask, objects_from_svg_file, page, \
+    path_effect, path, polygon, polyline, pop_defaults, push_defaults, \
+    radial_gradient, randcolor, rect, regular_polygon, save_file, \
+    selected_shapes, star, style, text, transform, z_sort
