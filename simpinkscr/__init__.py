5549a324dd1e403c892da68a7c78c9792252d1c1
Scott Pakin <scott+gpg@pakin.org>
2025-03-31 19:40:32 -0600

    Export the grid function

--- simpinkscr/__init__.py
+++ simpinkscr/__init__.py
@@ -5,7 +5,7 @@ from inkex.paths import Arc, Curve, Horz, Line, Move, Quadratic, Smooth, \
     TepidQuadratic, Vert, ZoneClose
 from .simple_inkscape_scripting import all_pages, all_shapes, \
     apply_action, apply_path_operation, arc, circle, clip_path, clone, \
-    connector, duplicate, ellipse, filter_effect, foreign, group, \
+    connector, duplicate, ellipse, filter_effect, foreign, grid, group, \
     guide, hyperlink, image, inkex_object, layer, linear_gradient, \
     line, main, marker, mask, objects_from_svg_file, page, \
     path_effect, path, polygon, polyline, pop_defaults, push_defaults, \
